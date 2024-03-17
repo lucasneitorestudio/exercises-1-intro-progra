33c4ec2ae739fed9f509f8dc85bc88b7ecd7fb33
lucasneitorestudio <159574448+lucasneitorestudio@users.noreply.github.com>
2024-03-16 22:24:12 -0400

    prueba 16

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -94,5 +94,37 @@ void exercise_15(int a, int b, int c) {
 }
 
 void exercise_16(int debut, int fin) {
-  // TODO: YOUR CODE HERE
+    if (debut < 0 || debut > 24 || fin < 0 || fin > 24) {
+        cout << "Las horas deben estar entre 0 y 24!" << endl;
+        return;
+    } else if (debut == fin) {
+        cout << "Que extraño, no has alquilado tu bicicleta por mucho tiempo!" << endl;
+        return;
+    } else if (debut > fin) {
+        cout << "Que extraño, el inicio del alquiler es después del final..." << endl;
+        return;
+    }
+
+    int costo_total = 0;
+    int hora_inicial = debut;
+    int hora_final = fin;
+    
+    // Calcula el costo para las horas de la mañana (de 00:00 a 07:00)
+    if (hora_inicial <= 7) {
+        costo_total += min(7, hora_final) - hora_inicial;
+    }
+    
+    // Calcula el costo para las horas de la tarde (de 17:00 a 24:00)
+    if (hora_final >= 17) {
+        costo_total += hora_final - max(17, hora_inicial);
+    }
+    
+    // Calcula el costo para las horas del día (de 07:00 a 17:00)
+    if (hora_inicial < 7 && hora_final > 7 && hora_final < 17) {
+        costo_total += hora_final - 7;
+    } else if (hora_inicial < 17 && hora_final > 17) {
+        costo_total += min(17, hora_final) - max(7, hora_inicial);
+    }
+    
+    cout << "El monto total a pagar es de " << costo_total << " boliviano(s)." << endl;
 }
